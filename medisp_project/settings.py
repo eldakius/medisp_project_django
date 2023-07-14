4a7c4634d5d1366dd3addc6dc9fd57c389beb8f0
Elda <bme18388030@uniwa.gr>
2023-07-14 20:38:32 +0300

    server setup using DRF

--- medisp_project/settings.py
+++ medisp_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "machine_learning.apps.MachineLearningConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
