462ca6d757e9a8fbe4626fe0058e1e6f20d1370b
Elda <bme18388030@uniwa.gr>
2023-07-14 22:45:04 +0300

    model changes

--- medisp_project/settings.py
+++ medisp_project/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+STORAGE_FOLDER = "medisp_storage"
+MEDIA_ROOT = os.path.join(BASE_DIR, STORAGE_FOLDER)
+HIST_IMAGES = os.path.join(MEDIA_ROOT, "hist_images")
+os.makedirs(HIST_IMAGES, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
